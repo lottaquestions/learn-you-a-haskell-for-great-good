fea1cd321f4133b1c7456f3f9b3dc4f904a49720
Nick Othieno <nickothieno@yahoo.com>
2022-03-19 15:13:40 -0400

    Complete chapter 4

--- chapter4_hello_recursion.hs
+++ chapter4_hello_recursion.hs
@@ -55,3 +55,18 @@ False
 5 `elem'` []
 False
 --}
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a | a <- xs, a > x]
+  in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+{--
+quicksort [10, 2, 5, 3,1,6,7,4,2,3,4,8,9]
+quicksort [10, 2, 5, 3,1,6,7,4,2,3,4,8,9]
+[1,2,2,3,3,4,4,5,6,7,8,9,10]
+*Main> quicksort "the quick brown fox jumps over the lazy dog"
+quicksort "the quick brown fox jumps over the lazy dog"
+"        abcdeeefghhijklmnoooopqrrsttuuvwxyz"
+--}
